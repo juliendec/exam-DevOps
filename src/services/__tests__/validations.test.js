6ad0136987559e33a760ef17695640d48b43c7a3
juliendec <juliendecoen2308@gmail.com>
2023-01-20 10:31:17 +0100

    test(redflag for special char): red flag and improve test of lenght

--- src/services/__tests__/validations.test.js
+++ src/services/__tests__/validations.test.js
@@ -20,16 +20,27 @@ describe("validations tests suites - isEmpty", () => {
 // TODO: Create tests suite for validation function
 
 describe("validations tests suites - isMinLenght", () => {
-    test("should return true as the label is less that 8 carac.", () => {
+    test("should return false as the label is less that 8 carac.", () => {
         const result = isMinLenght();
         expect(result).toBe(false);
     });
-    test("should return true as the label is less that 8 carac.", () => {
+    test("should return true as the label is more that 8 carac.", () => {
         const result = isMinLenght("GamerTag89");
         expect(result).toBe(true);
     });
     test("should return true as the label is less that 8 carac.", () => {
-        const result = isMinLenght("");
+        const result = isMinLenght("ded");
+        expect(result).toBe(false);
+    });
+});
+
+describe("validations tests suites - haveSpecialChar", () => {
+    test("should return true as the label have a special char", () => {
+        const result = haveSpecialChar("GamerTag@89");
+        expect(result).toBe(true);
+    });
+    test("should return false as the label don't have a special char.", () => {
+        const result = haveSpecialChar("gamersansrien");
         expect(result).toBe(false);
     });
 });
